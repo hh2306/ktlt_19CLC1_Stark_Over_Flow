e16ba8f129a47f67adde098bee096559574adc4e
revenour <19127428@student.hcmus.edu.vn>
2020-04-21 10:38:15 +0700

    fix

--- 19127428_19127409_19127518/Project_Stark_Over_Flow/Project_Stark_Over_Flow.cpp
+++ 19127428_19127409_19127518/Project_Stark_Over_Flow/Project_Stark_Over_Flow.cpp
@@ -1,57 +1,28 @@
 #include <iostream>
 #include "Projec.h"
-<<<<<<< HEAD
-<<<<<<< HEAD
-#include "Readmember.h"
-#include "LogIn.h"
-#include "chpassintxt.h"
-#include "changepassword.h"
-=======
 #include "LogIn.h"
 #include "Readmember.h"
->>>>>>> 347267266f849be3c597e8dd0de1f9f4d9ef0854
-=======
-#include "LogIn.h"
-#include "Readmember.h"
->>>>>>> 347267266f849be3c597e8dd0de1f9f4d9ef0854
 using namespace std;
 
 
 int main()
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    // khong dung vao day
-    // tao class rieng de lam
-  
-    loginresult F;
-    member user;
-    int n_staff, n_lec, n_stdnt;
-    Read_lecture(user.lec, n_lec);
-    Read_staff(user.stf, n_staff);
-    Read_student(user.stdnt, n_stdnt);
-
-    string username, password; // for logging in
-    F = login(username, password, user, n_staff, n_lec, n_stdnt);
-    if (chpass(user, password, F) == true)
-    {
-        chpassintxt(user, F, n_staff, n_lec, n_stdnt);
-    }
-=======
-=======
->>>>>>> 347267266f849be3c597e8dd0de1f9f4d9ef0854
     cout << "Login                 ( Press 1 )" << endl;
     cout << "Show Menu             ( Press 2 )" << endl;
     cout << "View Profile Info     ( Press 3 )" << endl;
     cout << "Change Password       ( Press 4 )" << endl;
     cout << "Logout                ( Press 5 )" << endl;
-
-
+    string username, pass;
+    member a;
+    loginresult F;
+    int n_lec,  n_staff,  n_stdnt;
+    Read_lecture(a.lec, n_lec);
+    Read_staff(a.stf, n_staff);
+    Read_student(a.stdnt, n_stdnt);
+    F = login(username, pass, a, n_lec, n_staff, n_stdnt);
+    cout << F.location << " " << F.type;
+    ViewProfileInfo(a, F);
     return 0;
 
-<<<<<<< HEAD
->>>>>>> 347267266f849be3c597e8dd0de1f9f4d9ef0854
-=======
->>>>>>> 347267266f849be3c597e8dd0de1f9f4d9ef0854
 }
 
--- 19127428_19127409_19127518/Project_Stark_Over_Flow/changepassword.h
+++ 19127428_19127409_19127518/Project_Stark_Over_Flow/changepassword.h
@@ -2,5 +2,6 @@
 #include <iostream>
 #include "Projec.h"
 using namespace std;
-bool chpass(member& user, string password, loginresult F); //F has the location of that array which holds the infomation about the user by the login function
+void chpass(member& user, string password, loginresult F); //F has the location of that array which holds the infomation about the user by the login function
+
 
